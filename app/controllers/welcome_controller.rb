ec61deba40d2d2798c2ec4860fee28fe977b8b5a
toshabrown <brown_tosh@live.com>
2014-03-25 21:47:26 -0400

    Welcome

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,9 @@
+class WelcomeController < ApplicationController
+  def index
+    <h1>" Welcome to Creative Loop!"</h1>
+
+  end
+
+  def about
+   <h1> "Creative Loop is a place for friends"</h1> 
+end
